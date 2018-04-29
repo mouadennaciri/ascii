aacd2aa4fcc187e8165e8130af3266d1d78a58df
Mouad Ennaciri <mouad420@gmail.com>
2018-04-29 02:09:09 +0100

    Update

---
+++ frontend/components/hello.js
@@ -0,0 +1,11 @@
+var React = require('react')
+
+module.exports = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>Bonjour le monde</h1>
+      </div>
+    )
+  }
+})
